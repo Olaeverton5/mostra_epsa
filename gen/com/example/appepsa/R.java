a04216ba34c1b19bd473c7384cf784a01f37fef7
Luciano-Santos <lslucianossantos@gmail.com>
2019-04-25 13:06:10 -0300

    Troca de icone e definição de id's

--- gen/com/example/appepsa/R.java
+++ gen/com/example/appepsa/R.java
@@ -24,14 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int bTcadastrar=0x7f080002;
-        public static final int bTentrar=0x7f080000;
-        public static final int eTemail=0x7f080006;
-        public static final int eTsenha=0x7f080003;
-        public static final int tVemail=0x7f080005;
-        public static final int tVesquecisenha=0x7f080001;
-        public static final int tVsenha=0x7f080004;
+        public static final int action_settings=0x7f080008;
+        public static final int bTcadastre=0x7f080006;
+        public static final int bTentrar=0x7f080005;
+        public static final int eTemail=0x7f080001;
+        public static final int eTsenha=0x7f080004;
+        public static final int tVemail=0x7f080003;
+        public static final int tVesquecisenha=0x7f080007;
+        public static final int tVsejabemvindo=0x7f080002;
+        public static final int tVsenha=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -42,7 +43,15 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int bTcadastre=0x7f05000d;
+        public static final int bTentrar=0x7f05000c;
+        public static final int bemvindo=0x7f05000f;
         public static final int cadastre=0x7f050003;
+        public static final int eTappepsa=0x7f050008;
+        public static final int eTbemvindo=0x7f05000e;
+        public static final int eTemail=0x7f050009;
+        public static final int eTesquecisenha=0x7f05000b;
+        public static final int eTsenha=0x7f05000a;
         public static final int email=0x7f050007;
         public static final int entrar=0x7f050004;
         public static final int esqueci=0x7f050005;
